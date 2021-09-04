0d1cda136dce025ff23b99d85fc4a5dab2eb1528
bao21092001 <baolnpc01028@fpt.edu.vn>
2021-09-04 20:48:20 +0700

    first commit

---
+++ Baolnpc01028_lab7_java6/src/main/java/com/baolnpc01028_lab7/Baolnpc01028Lab7Java6Application.java
@@ -0,0 +1,13 @@
+package com.baolnpc01028_lab7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Baolnpc01028Lab7Java6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Baolnpc01028Lab7Java6Application.class, args);
+	}
+
+}
---
+++ Baolnpc01028_lab7_java6/src/test/java/com/baolnpc01028_lab7/Baolnpc01028Lab7Java6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baolnpc01028_lab7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Baolnpc01028Lab7Java6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
